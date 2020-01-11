b773378e50dad3479fc8136d65df23e8fd6d6e0f
AlanChu-OU <g1186214@study.ouhk.edu.hk>
2020-01-12 00:44:54 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphQLHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.use(
+    '/graphql', 
+    graphQLHttp({
+        schema: graphQlSchema,
+        rootValue: graphQlResolvers,
+        graphiql: true
+    })
+);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+}@cluster0-ebi7i.mongodb.net/${
+    process.env.MONGO_DB}?retryWrites=true&w=majority`
+).then(()=>{
+    app.listen(8099);   //connect to db, then turn on server
+}).catch(err=>{
+    console.log(err);
+});
+
