a0559c6cfef8928aea12c13f49081906f8347b0f
AlanChu-OU <g1186214@study.ouhk.edu.hk>
2020-01-17 00:30:16 +0800

    Auth.css

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
+import './Auth.css';
+
 class AuthPage extends Component {
     render(){
         return(
-            <h1>The Auth Page</h1>
+            <form className="auth-form ">
+                <div className="form-control">
+                    <label htmlFor="email">E-Mail</label>
+                    <input type="email" id="email"/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="pwd">Password</label>
+                    <input type="password" id="pwd"/>
+                </div>
+                <div className="form-actions">
+                    <button type="button">Switch to Signup</button>
+                    <button type="submit">Login</button>    
+                </div>
+            </form>
         );
     }
 }
