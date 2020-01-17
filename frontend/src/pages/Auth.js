4c57e7ec3ffa4553fcb23034cd2bc876363048d3
AlanChu-OU <g1186214@study.ouhk.edu.hk>
2020-01-18 00:29:50 +0800

    Signup and login

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -3,53 +3,73 @@ import React, { Component } from 'react';
 import './Auth.css';
 
 class AuthPage extends Component {
-constructor(props){
-    super(props);
-    this.emailEl = React.createRef();
-    this.passwordEl = React.createRef();
-}
-
-submitHendler = (event) =>{
     state = {
         isLogin: true
     };
 
-    event.preventDefault();
-    const email = this.emailEl.current.value;
-    const password = this.passwordEl.current.value;
-
-    if(email.trim().length === 0 || password.trim().length === 0){
-        return;
+    constructor(props){
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
     }
 
-    const requestBody = {
-        query: `
-            mutation{
-                createUser(userInput: {email: "${email}", password: "${password}"}){
-                    _id
-                    email
-                }
-            }
-            `
-    };
+    switchModeHandler = () => {
+        this.setState(prevState => {
+            return {isLogin: !prevState.isLogin};
+        })
+    }
 
-    fetch('http://localhost:8099/graphql', {
-        method: 'POST',
-        body: JSON.stringify(requestBody),
-        headers: {
-            'Content-Type': 'application/json'
+    submitHendler = (event) => {
+        event.preventDefault();
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if(email.trim().length === 0 || password.trim().length === 0){
+            return;
         }
-    }).then(res => {
-        if(res.status !== 200 && res.status !== 201){
-            throw new Error('Failed!');
+
+        let requestBody = {
+            query: `
+                query {
+                    login(email: "${email}", password: "${password}"){
+                        userId
+                        token
+                        tokenExpiration
+                    }
+                }
+            `
+        };
+
+        if(!this.state.isLogin){
+            requestBody = {
+                query: `
+                    mutation{
+                        createUser(userInput: {email: "${email}", password: "${password}"}){
+                            _id
+                            email
+                        }
+                    }
+                    `
+            };
         }
-        return res.json();
-    }).then(resData => {
-        console.log(resData);
-    }).catch(err => {
-        console.log(err);
-    });
-};
+
+        fetch('http://localhost:8099/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if(res.status !== 200 && res.status !== 201){
+                throw new Error('Failed!');
+            }
+            return res.json();
+        }).then(resData => {
+            console.log(resData);
+        }).catch(err => {
+            console.log(err);
+        });
+    };
 
     render(){
         return(
@@ -64,7 +84,9 @@ submitHendler = (event) =>{
                 </div>
                 <div className="form-actions">
                     <button type="submit">Submit</button>  
-                    <button type="button">Switch to Login</button>  
+                    <button type="button" onClick={this.switchModeHandler}>
+                        Switch to {this.state.isLogin ? 'Signup' : 'Login'}
+                    </button>  
                 </div>
             </form>
         );
